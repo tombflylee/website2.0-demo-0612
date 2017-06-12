66ede3b89e7539e88ccc97af1293aad920c4d771
sx-9253 <tombflylee@hotmail.com>
2017-06-12 17:29:50 +0800

    0612

--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux'
 import App from './containers/App'
 
 render(
-	<Provider>
+	<Provider store={store}>
 		<App />
-	</Provider>,document.getElementById("app"))
+	</Provider>,document.getElementById("app")
+)
---
+++ app/reducers/ui.js
@@ -0,0 +1,9 @@
+import { UI_INITIAL_STATE } from '../constants/initial-state'
+
+const initialState = 
+export const ui = (state = initialState,action) =>{
+	switch(action.type){
+		case FETCH_UI:
+
+	}
+}
